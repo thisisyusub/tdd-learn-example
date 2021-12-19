758a03616d49a202617f7d3e00c3b13d20d8f6a9
Kanan Yusubov <kenan.yusubov@stargroup.az>
2021-12-19 19:36:10 +0400

    get users use case test done!

--- test/features/get_users/domain/usecases/get_users_use_case_test.dart
+++ test/features/get_users/domain/usecases/get_users_use_case_test.dart
@@ -1,47 +1,42 @@
 import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tdd_example/features/get_users/domain/entities/user.dart';
 import 'package:tdd_example/features/get_users/domain/repositories/i_user_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:tdd_example/features/get_users/domain/usecases/get_users.dart';
 
-class MockUsersRepository extends Mock implements IUserRepository {}
+import 'get_users_use_case_test.mocks.dart';
 
+@GenerateMocks([IUserRepository])
 void main() {
   late GetUsers usecase;
-  late MockUsersRepository mockUsersRepository;
-  late final List<User> users;
+  late MockIUserRepository mockUsersRepository;
+  late List<User> users;
 
   group('GetUsers Usecase', () {
     setUp(() {
-      mockUsersRepository = MockUsersRepository();
+      mockUsersRepository = MockIUserRepository();
       usecase = GetUsers(mockUsersRepository);
+
+      users = const <User>[
+        User(
+          id: 1,
+          name: 'Test User 1',
+          username: 'Test Username 1',
+          email: 'testuser1@gmail.com',
+        ),
+      ];
     });
 
     test(
       'should get users from the repository',
       () async {
-        users = const <User>[
-          User(
-            id: 1,
-            name: 'User 1',
-            username: 'User 1',
-            email: 'user1@gmail.com',
-          ),
-          User(
-            id: 2,
-            name: 'User 2',
-            username: 'User 2',
-            email: 'user2@gmail.com',
-          ),
-        ];
-
         when(mockUsersRepository.getUsers()).thenAnswer(
           (_) async => Right(users),
         );
 
         final result = await usecase.execute();
-        print('result: $result');
 
         expect(result, Right(users));
         verify(mockUsersRepository.getUsers());
