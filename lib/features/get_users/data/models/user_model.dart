ddb071ea32940670004d3f1be57315a9776d6fdc
Kanan Yusubov <kenan.yusubov@stargroup.az>
2021-12-20 10:21:59 +0400

    get user by id test chaged

--- lib/features/get_users/data/models/user_model.dart
+++ lib/features/get_users/data/models/user_model.dart
@@ -1,4 +1,4 @@
-import 'package:tdd_example/features/get_users/domain/entities/user.dart';
+import '../../domain/entities/user.dart';
 
 class UserModel extends User {
   const UserModel({
--- test/features/get_users/domain/usecases/get_user_by_id_test.dart
+++ test/features/get_users/domain/usecases/get_user_by_id_test.dart
@@ -2,13 +2,11 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:tdd_example/features/get_users/domain/repositories/user_repository.dart';
 import 'package:tdd_example/features/get_users/domain/usecases/get_user_by_id.dart';
 
 import 'mock_user_repository.mocks.dart';
 import '../../../../core/mocks/mock_users.dart';
 
-@GenerateMocks([UserRepository])
 void main() {
   late MockUserRepository mockUserRepository;
   late GetUserById usecase;
