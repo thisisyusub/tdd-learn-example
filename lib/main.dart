5b8c0c75a2aa963e87e3c9b4fe4a0f781807bac5
Kanan Yusubov <kenan.yusubov@stargroup.az>
2021-12-22 19:35:13 +0400

    changed

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.green,
       ),
       home: const UsersPage(),
     );
