0b20fa2c4c1d20446fed13465e74da4fa1fdc76b
Kanan Yusubov <kenan.yusubov@stargroup.az>
2021-12-23 12:15:59 +0400

    primary swatch changed for codecov test

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.brown,
       ),
       home: const UsersPage(),
     );
