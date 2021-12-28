73b5088cd09999888f58a567b85f0d7fb4c173f3
Thisisyusub <kanan.yusub@gmail.com>
2021-12-28 09:02:48 +0400

    DI completed

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -4,11 +4,16 @@ import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/network/network_info.dart';
+import 'data/data_sources/post_remote_data_source.dart';
 import 'data/data_sources/user_local_data_source.dart';
 import 'data/data_sources/user_remote_data_source.dart';
+import 'data/repositories/post_repository_impl.dart';
 import 'data/repositories/user_repository_impl.dart';
+import 'domain/repositories/post_repository.dart';
 import 'domain/repositories/user_repository.dart';
+import 'domain/use_cases/get_user_posts.dart';
 import 'domain/use_cases/get_users.dart';
+import 'presentation/bloc/user_posts_cubit.dart';
 import 'presentation/bloc/users_cubit.dart';
 
 final getIt = GetIt.instance;
@@ -24,10 +29,12 @@ Future<void> init() async {
 
 void initBlocs() {
   getIt.registerFactory(() => UsersCubit(getIt()));
+  getIt.registerFactory(() => UserPostsCubit(getIt()));
 }
 
 void initUseCases() {
   getIt.registerLazySingleton(() => GetUsers(getIt()));
+  getIt.registerLazySingleton(() => GetUserPosts(getIt()));
 }
 
 void initRepositories() {
@@ -38,6 +45,10 @@ void initRepositories() {
       networkInfo: getIt(),
     ),
   );
+
+  getIt.registerLazySingleton<PostRepository>(
+    () => PostRepositoryImpl(getIt()),
+  );
 }
 
 void initDataSources() {
@@ -47,6 +58,10 @@ void initDataSources() {
 
   getIt.registerLazySingleton<UserLocalDataSource>(
     () => UserLocalDataSourceImpl(getIt()),
+  );
+
+  getIt.registerLazySingleton<PostRemoteDataSource>(
+    () => PostRemoteDataSourceImpl(getIt()),
   );
 }
 
