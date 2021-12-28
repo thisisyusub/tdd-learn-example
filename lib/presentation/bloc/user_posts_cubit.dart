c79c17e9a388c46d0aede47bbb7d4149034baaeb
Thisisyusub <kanan.yusub@gmail.com>
2021-12-28 08:59:08 +0400

    user posts cubit completed

--- lib/presentation/bloc/user_posts_cubit.dart
+++ lib/presentation/bloc/user_posts_cubit.dart
@@ -4,8 +4,8 @@ import '../../core/states/data_state.dart';
 import '../../domain/entities/post.dart';
 import '../../domain/use_cases/get_user_posts.dart';
 
-class PostsCubit extends Cubit<DataState<List<Post>>> {
-  PostsCubit(this.getUserPosts) : super(DataState.initial());
+class UserPostsCubit extends Cubit<DataState<List<Post>>> {
+  UserPostsCubit(this.getUserPosts) : super(DataState.initial());
 
   final GetUserPosts getUserPosts;
 
--- test/presentation/bloc/user_posts_cubit_test.dart
+++ test/presentation/bloc/user_posts_cubit_test.dart
@@ -5,7 +5,7 @@ import 'package:mockito/mockito.dart';
 import 'package:tdd_example/core/error/failure.dart';
 import 'package:tdd_example/core/states/data_state.dart';
 import 'package:tdd_example/domain/entities/post.dart';
-import 'package:tdd_example/presentation/bloc/posts_cubit.dart';
+import 'package:tdd_example/presentation/bloc/user_posts_cubit.dart';
 
 import '../../mocks/generate_mocks.mocks.dart';
 import '../../mocks/mock_posts.dart';
@@ -13,29 +13,29 @@ import '../../mocks/mock_posts.dart';
 typedef Posts = List<Post>;
 
 void main() {
-  late PostsCubit postsCubit;
+  late UserPostsCubit postsCubit;
   late MockGetUserPosts mockGetUserPosts;
 
   setUp(() {
     mockGetUserPosts = MockGetUserPosts();
-    postsCubit = PostsCubit(mockGetUserPosts);
+    postsCubit = UserPostsCubit(mockGetUserPosts);
   });
 
-  group('PostsCubit', () {
-    blocTest<PostsCubit, DataState<Posts>>(
+  group('UserPostsCubit', () {
+    blocTest<UserPostsCubit, DataState<Posts>>(
       'should emit empty if nothing happened',
       build: () => postsCubit,
       expect: () => [],
     );
 
-    blocTest<PostsCubit, DataState<Posts>>(
+    blocTest<UserPostsCubit, DataState<Posts>>(
       'should emit [InProgress] when action is started',
       build: () => postsCubit,
       act: (cubit) => cubit.fetchUserPosts(1),
       expect: () => [DataState<Posts>.inProgress()],
     );
 
-    blocTest<PostsCubit, DataState<Posts>>(
+    blocTest<UserPostsCubit, DataState<Posts>>(
       'should emit [Failure] when action is not successful',
       setUp: () {
         when(mockGetUserPosts.call(any)).thenAnswer(
@@ -50,7 +50,7 @@ void main() {
       ],
     );
 
-    blocTest<PostsCubit, DataState<Posts>>(
+    blocTest<UserPostsCubit, DataState<Posts>>(
       'should emit [Empty] if there is no user',
       setUp: () {
         when(mockGetUserPosts.call(any)).thenAnswer(
@@ -65,7 +65,7 @@ void main() {
       ],
     );
 
-    blocTest<PostsCubit, DataState<Posts>>(
+    blocTest<UserPostsCubit, DataState<Posts>>(
       'should emit [Success] with the list of users when action is  successful',
       setUp: () {
         when(mockGetUserPosts.call(any)).thenAnswer(
